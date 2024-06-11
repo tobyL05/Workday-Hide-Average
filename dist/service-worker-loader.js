49e89b353cd82ea2b5951b32256a5be8de655e8c
Toby <79076160+tobyL05@users.noreply.github.com>
2024-06-10 23:43:02 -0700

    fixes

--- dist/service-worker-loader.js
+++ dist/service-worker-loader.js
@@ -1,3 +1,1 @@
-import 'http:/localhost:3000/@vite/env';
-import 'http://localhost:3000/crx-client-worker';
-import 'http://localhost:3000/src/background.ts';
+import './assets/background.ts.985d3ec2.js';
